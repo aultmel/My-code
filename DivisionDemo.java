21d7bd172774303c1f5b88a0e3e5b2c723f13d68
aultmel <114507910+aultmel@users.noreply.github.com>
2024-02-08 19:10:11 -0600

    Add files via upload

---
+++ DivisionDemo.java
@@ -0,0 +1,14 @@
+public class DivisionDemo {
+	public static void main(String[] args) {
+		System.out.println(2.0/5.0);
+		System.out.println(2.0/5);
+		System.out.println(2/5.0);
+		System.out.println(2/5);
+		System.out.println(32/5);
+		System.out.println(888/889);
+		System.out.println(2%5);
+		System.out.println(34%5);
+		System.out.println(888%889);
+		
+	}
+}
