adbccd8fd27a56ff539ee3323c3b2611fa3fa728
aultmel <114507910+aultmel@users.noreply.github.com>
2024-02-08 19:11:18 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+This is a simple example of a Hello World program in Java.
+I could also have many other lines of comments here.
+And the compiler would ignore them all.
+*/
+
+public class HelloWorld {	// This marks the beginning of the program
+	public static void main(String[] args) {	// This marks the beginning of the main method
+
+		// Here are some examples of using print and println
+			System.out.print("Hello world!");
+			System.out.println("Here is something else");
+	} 
+}
